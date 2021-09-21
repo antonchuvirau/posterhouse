56ae518b1219e56766d99e86e004a43dc8602b07
Valentin Anikin <anival-94@mail.ru>
2021-09-21 19:41:43 +0300

    add controller for paperTypes modal

--- src/controllers/PaperTypesModalController.js
+++ src/controllers/PaperTypesModalController.js
@@ -2,87 +2,74 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 const PaperTypesModalController = () => {
-    // const [isModalOpened, setIsModalOpened] = useState(false);
+    const [isModalOpened, setIsModalOpened] = useState(false);
 
-    // const openBtn = useRef(null);
-    // const closeBtn = useRef(null);
+    const openBtn = useRef(null);
+    const closeBtn = useRef(null);
+    const backDrop = useRef(null);
+    const modal = useRef(null);
 
-    // const onModalClose = useCallback((e) => {
-    //     e.stopPropagation();
-    //     setIsModalOpened(false);
-    // }, []);
+    const onModalClose = useCallback((e) => {
+        e.stopPropagation();
+        setIsModalOpened(false);
+    }, []);
 
-    // const onModalOpen = useCallback(() => {
-    //     setIsModalOpened(true);
-    // }, []);
-
-    // // paper-types-modal-btn
-    // const continueShoppingBtn = useRef(null);
-    // const openWishListSidePanelBtn = useRef(null);
-    // const backDropComponent = useRef(null);
-    // const wishListSidePanel = useRef(null);
-
-    // const toggleWishSidePanelVisibility = useCallback(() => {
-    //     if (!wishListSidePanel.current || !backDropComponent.current) {
-    //         return;
-    //     }
-
-    //     wishListSidePanel.current.classList.toggle('wish-list-side-panel--state_invisible');
-    //     backDropComponent.current.classList.toggle('wish-list-side-panel__backdrop--state-invisible');
-    // }, [])
+    const onModalOpen = useCallback(() => {
+        setIsModalOpened(true);
+    }, []);
 
 
-    // useEffect(() => {
-    //     toggleWishSidePanelVisibility();
-    // }, [isWishSidePanelOpen, toggleWishSidePanelVisibility])
+    const toggleModalVisibility = useCallback(() => {
+        if (!modal.current || !backDrop.current) {
+            return;
+        }
+
+        modal.current.classList.toggle('paper-types-modal--state-invisible');
+        backDrop.current.classList.toggle('paper-types-modal__backdrop--state-invisible');
+    }, [])
+
+
+    useEffect(() => {
+        toggleModalVisibility();
+    }, [isModalOpened, toggleModalVisibility])
 
 
 
-    // useEffect(() => {
-    //     openBtn.current = document.getElementById('paper-types-modal-btn');
-    //     closeBtn.current = document.getElementById('paper-types-modal__close-btn');
+    useEffect(() => {
+        openBtn.current = document.getElementById('paper-types-modal-btn');
+        closeBtn.current = document.getElementById('paper-types-modal__close-btn');
+        backDrop.current = document.getElementById('paper-types-modal__backdrop');
+        modal.current = document.getElementById('paper-types-modal');
 
-    //     if (openBtn.current) {
-    //         openBtn.current.addEventListener('click', onModalOpen);
-    //     }
+        if (openBtn.current) {
+            openBtn.current.addEventListener('click', onModalOpen);
+        }
 
-    //     if (closeBtn.current) {
-    //         closeBtn.current.addEventListener('click', onModalClose);
-    //     }
+        if (closeBtn.current) {
+            closeBtn.current.addEventListener('click', onModalClose);
+        }
 
+        if (backDrop.current) {
+            backDrop.current.addEventListener('click', onModalClose);
+        }
 
+        return () => {
+            if (closeBtn.current) {
+                closeBtn.current.removeEventListener('click', onModalClose);
+            }
 
+            if (openBtn.current) {
+                openBtn.current.removeEventListener('click', onModalOpen);
+            }
 
-
-
-    //     continueShoppingBtn.current = document.getElementById('wish-list-side-panel__continue-shopping-btn');
-    //     openWishListSidePanelBtn.current = document.getElementById('open-wish-list-side-panel');
-    //     backDropComponent.current = document.getElementById('wish-list-side-panel__backdrop');
-    //     wishListSidePanel.current = document.getElementById('wish-list-side-panel');
-
-    //     if (closeBtn.current) {
-    //         closeBtn.current.addEventListener('click', onWishSidePanelClose);
-    //     }
-
-    //     if (continueShoppingBtn.current) {
-    //         continueShoppingBtn.current.addEventListener('click', onWishSidePanelClose);
-    //     }
-
-    //     if (openWishListSidePanelBtn.current) {
-    //         openWishListSidePanelBtn.current.addEventListener('click', onWishSidePanelOpen);
-    //     }
-
-    //     if (backDropComponent.current) {
-    //         backDropComponent.current.addEventListener('click', onWishSidePanelClose);
-    //     }
-
-    //     return () => {
-    //         closeBtn.current.removeEventListener('click', onWishSidePanelClose);
-    //         continueShoppingBtn.current.removeEventListener('click', onWishSidePanelClose);
-    //         openWishListSidePanelBtn.current.removeEventListener('click', onWishSidePanelOpen);
-    //         backDropComponent.current.removeEventListener('click', onWishSidePanelClose);
-    //     };
-    // }, [onWishSidePanelClose, onWishSidePanelOpen]);
+            if (backDrop.current) {
+                backDrop.current.removeEventListener('click', onModalClose);
+            }
+        };
+    }, [
+        onModalClose,
+        onModalOpen,
+    ]);
 
     return null;
 };
